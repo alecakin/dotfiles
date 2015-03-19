1793ffb0b08b7722fbcb3f44c6cb9b9f2001baf6
Alex Dean <alexdean@alexdeanvmlx.ad.selinc.com>
2015-03-19 16:06:09 -0700

    Updated install script

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -5,27 +5,27 @@ if [ -e ~/.vimrc ]
 then
     echo ".vimrc exists, moving it to .vimrc.bak"
     mv ~/.vimrc ~/.vimrc.bak
-    ln -s $DIR/vimrc ~/.vimrc
 fi
+ln -s $DIR/vimrc ~/.vimrc
 
 if [ -e ~/.bashrc ]
 then
     echo ".bashrc exists, moving it to .bashrc.bak"
     mv ~/.bashrc ~/.bashrc.bak
-    ln -s $DIR/bashrc ~/.bashrc
 fi
+ln -s $DIR/bashrc ~/.bashrc
 
 if [ -e ~/.inputrc ]
 then
     echo ".inputrc exists, moving it to .inputrc.bak"
     mv ~/.inputrc ~/.inputrc.bak
-    ln -s $DIR/inputrc ~/.inputrc
 fi
+ln -s $DIR/inputrc ~/.inputrc
 
 if [ -e ~/.tmux.conf ]
 then
     echo ".tmux.conf exists, moving it to .tmux.conf.bak"
     mv ~/.tmux.conf ~/.tmux.conf.bak
-    ln -s $DIR/tmux.conf ~/.tmux.conf
 fi
+ln -s $DIR/tmux.conf ~/.tmux.conf
 
